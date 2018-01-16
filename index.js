cba30bc6da72811b683bfc4dda6867ba83d21802
Ben Deutsch <ben.deutschiii@gmail.com>
2018-01-16 17:57:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var price = Math.floor((Math.random() * 100) + 1);
+ var newItem = {[item]: price};
+ cart.push(newItem);
+ console.log(`${item} has been added to your cart.`);
+ return cart;
 }
 
 function viewCart() {
